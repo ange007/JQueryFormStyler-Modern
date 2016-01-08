0fecc1aa262490e186963b2153838c52611ff337
ange007 <ange007@ange007-work>
2016-01-08 16:21:32 +0200

    v1.1.5

--- src/script/_radio.js
+++ src/script/_radio.js
@@ -34,35 +34,9 @@ var radioOutput = function( )
 	// Клик по псевдоблоку
 	radio.click( function( e )
 	{
+		//
 		e.preventDefault( );
 
-		// Обрабатываем только активную радиокнопку
-		if( !radio.is( '.disabled' ) )
-		{			
-			// Передаём фокус и вызываем событие - изменения
-			el.focus( )
-				.change( );
-		}
-	} );
-	
-	// Клик на label
-	el.closest( 'label' ).add( 'label[for="' + el.attr( 'id' ) + '"]' ).on( 'click.' + pluginName, function( e )
-	{
-		if( !$( e.target ).is( 'a' ) && !$( e.target ).closest( radio ).length )
-		{
-			radio.triggerHandler( 'click' );
-			e.preventDefault( );
-		}
-	} );
-	
-	// Переключение стрелками
-	el.on( 'change.' + pluginName, function( )
-	{
-		if( radio.is( '.disabled' ) )
-		{
-			return;
-		}
-		
 		// Ищем нужный нам елемент в блоке который указан в настройках ( по умолчанию form )
 		var findElement = radio.closest( opt.wrapper )
 								.find( 'input[name="' + el.attr( 'name' ) + '"]:radio' );
@@ -84,21 +58,58 @@ var radioOutput = function( )
 		findElement.prop( 'checked', false )
 					.parent( ).removeClass( 'checked' );			
 
-		// Отмечаем 
-		el.prop( 'checked', true )
-				.parent( ).addClass( 'checked' );
+		// Обрабатываем только активную радиокнопку
+		if( !radio.is( '.disabled' ) )
+		{
+			// Передаём фокус и вызываем событие - изменения
+			el.prop( 'checked', true )
+				.focus( )
+				.change( );
+		}
+	} );
+		
+	// Переключение стрелками
+	el.on( 'change.' + pluginName, function( e )
+	{
+		if( !el.is( ':disabled' ) )
+		{
+			if( $( this ).is(':checked') )
+			{
+				el.parent( )
+					.addClass( 'checked' );
+			}
+			else
+			{
+				el.parent( )
+					.removeClass( 'checked' );
+			}
+		}
+		
 	} )
 	// Обработка наведения фокуса
 	.on( 'focus.' + pluginName, function( )
 	{
 		if( !radio.is( '.disabled' ) )
-
+		{
 			radio.addClass( 'focused' );
+		}
 	} )
 	// Обработка снятия фокуса
 	.on( 'blur.' + pluginName, function( )
 	{
 		radio.removeClass( 'focused' );
+	} );
+	
+	// Клик на label
+	el.closest( 'label' )
+		.add( 'label[for="' + el.attr( 'id' ) + '"]' )
+		.on( 'click.' + pluginName, function( e )
+	{
+		if( !$( e.target ).is( 'a' ) && !$( e.target ).closest( radio ).length )
+		{
+			radio.triggerHandler( 'click' );
+			e.preventDefault( );
+		}
 	} );
 	
 	// Мы установили стиль, уведомляем об изменении
@@ -112,7 +123,9 @@ radioOutput( );
 el.on( 'refresh', function( )
 {
 	//
-	el.closest( 'label' ).add( 'label[for="' + el.attr( 'id' ) + '"]' ).off( '.' + pluginName );
+	el.closest( 'label' )
+		.add( 'label[for="' + el.attr( 'id' ) + '"]' )
+		.off( '.' + pluginName );
 
 	// Убираем стилизацию компонента
 	el.off( '.' + pluginName )
