9fa4601461fa65a8aaf23e8f9d97b8782a08609c
ange007 <ange007@ange007-work>
2016-01-22 13:41:47 +0200

    Автоматический коммит в ветку release

--- .travis-release-commit.sh
+++ .travis-release-commit.sh
@@ -2,6 +2,7 @@
 # encoding: utf-8
  
 DIST_PATH=${TRAVIS_BUILD_DIR}/actual-release
+VERS=$(cat package.json && | jq '.version')
 # переходим в директорию
 cd ${TRAVIS_BUILD_DIR};
 # копируем в неё репозиторий
@@ -12,8 +13,16 @@ cd ${DIST_PATH} && git checkout release && rm -rf *;
 cp -rp ${TRAVIS_BUILD_DIR}/package.json ${DIST_PATH};
 cp -rp ${TRAVIS_BUILD_DIR}/composer.json ${DIST_PATH};
 cp -rp ${TRAVIS_BUILD_DIR}/bower.json ${DIST_PATH};
+cp -rp ${TRAVIS_BUILD_DIR}/README.md ${DIST_PATH};
 cp -rp ${TRAVIS_BUILD_DIR}/build/ ${DIST_PATH};
 # переходим в директорию добавляе коммит
 cd ${DIST_PATH} && git add -A && git commit -am "Автоматическая сборка (${TRAVIS_BUILD_NUMBER})";
-# отправляем коммит
-git push ${REPO_URL} release;
+# создаём тег и отправляем коммит
+if [[ $TRAVIS_BRANCH == 'master' ]]; then 
+	git tag -a v${VERS} -m "Релиз версии ${VERS}"; 
+	git push ${REPO_URL} release --tags;
+fi
+# или просто отправляем коммит
+if [[ $TRAVIS_BRANCH != 'master' ]]; then 
+	git push ${REPO_URL} release;
+fi
