6d59aec786bb1eeed6431c81f03a4b993ea09267
Vladimir B. <ange305@gmail.com>
2017-06-02 14:24:35 +0300

    v2.0.0

--- src/script/main.js
+++ src/script/main.js
@@ -208,14 +208,6 @@
 				// Стилизируем компонент
 				if( element.is( '[multiple]' ) || element.attr( 'size' ) > 1 )
 				{
-					// Если Android или iOS, то мультиселект не стилизуем
-					// Причина для Android: в стилизованном селекте нет возможности выбрать несколько пунктов
-					// Причина для iOS: в стилизованном селекте неправильно отображаются выбранные пункты
-					/*if( Android || iOS )
-					{
-						return;
-					}*/
-
 					this.customElement = new SelectBoxMulti( element, this.options.select, this.locales.select );
 				} 
 				else
